4aff5e5a4280f48916f6fd70ea801166cabf4132
Dipanshu-89 <ag7950675@gmail.com>
2026-02-04 01:01:06 +0530

    this file contains string and string methods programs

---
+++ stringmethods.py
@@ -0,0 +1,40 @@
+# str="hey\nbuddy\nyou\nare\nGamer"
+# print(str)
+# str='''mangoes "are sweets fruit'''
+# print(str)
+# str='mangoes "are sweets fruit'
+# print(str)                 #also use \ backslash symbol in double quotes
+# str=input("enter your string:")
+# print(str)
+# str="everything is possible"
+# print(len(str))
+# print(str[0:10])
+# print(str[14:22])
+# print(str[-22:-12])
+# print(str[len(str)-22:len(str)-12])
+# print(str[-1:])
+# string_methods
+# strings are immutable
+# str="!!hey i am your king!!!"
+# print(str.upper())
+# print(str)
+# print(str.lower())
+# print(str.rstrip("!"))
+# print(str.lstrip("!"))
+# print(str.replace("king","dipanshu"))
+# print(str.split(" "))
+# print(str.count("king"))
+# print(str.find("king"))
+# print(str.index("king"))
+# print(str.capitalize())
+# print(str.title())
+# print((str.center(25)))
+# print(str.isalnum())
+# print(str.isalpha())
+# print(str.isprintable())
+# print(str.startswith("!"))
+# print(str.endswith("!"))
+# print(str.isupper())
+# print(str.islower())
+# print(str.istitle())
+# print(str.isspace())
